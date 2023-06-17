e72b67f78bfd56ef7378fc923a82a5dd29b8b62a
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-06-17 11:46:13 +0100

    Initial commit

---
+++ JSON decoder/JSON decoder/JSON_decoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSON_decoderApp.swift
+//  JSON decoder
+//
+//  Created by Jonathan Copeland on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct JSON_decoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
